f3175c5afa41feaf0bb20a9c127d5502afd9188d
Ramzi Nofal <ramzis.mailbox@gmail.com>
2020-08-15 21:25:10 +0100

    Room schema (default apiKey value added)

--- models/room.js
+++ models/room.js
@@ -1,18 +1,20 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var roomSchema = mongoose.Schema;
 
-var schema = new roomSchema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  state: {
-    type: String,
-    enum : ['closed','opening-request','opened'],
-    default: 'closed'
-},
-  apiKey: String,
-  lastSeen: Date,
-  accepptedUsers: [String]
-},
- { collection : 'room-data' });
+var schema = new roomSchema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    state: {
+      type: String,
+      enum: ["closed", "opening-request", "opened"],
+      default: "closed",
+    },
+    apiKey: { type: String, default: "setme" },
+    lastSeen: Date,
+    accepptedUsers: [String],
+  },
+  { collection: "room-data" }
+);
 
-module.exports = mongoose.model('Room', schema);
+module.exports = mongoose.model("Room", schema);
