da81e885120f97c417a308877b38c5e503d07a00
Ramzi Nofal <ramzis.mailbox@gmail.com>
2021-01-31 02:10:49 +0000

    React - Get user update

--- routes/index.js
+++ routes/index.js
@@ -438,7 +438,7 @@ router.post("/reactLogin", function (req, res) {
 // GET USER ON REACT SITE (mongoose)
 ===================================================*/
 router.get("/reactGetUser", ensureToken, function (req, res) {
-  ReactUserOne.find({ email: req.body.email }, function (err, user) {
+  ReactUser.findOne({ email: req.body.email }, function (err, user) {
     var userFound = user;
     res.status(200).json(userFound);
   });
